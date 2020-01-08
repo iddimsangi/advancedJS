bc34d49049b69b6bbdc643ef8e505a74d6f2dd3c
iddimsangi <adammsangi@gmail.com>
2020-01-08 16:01:23 +0300

    create a functional constructor

---
+++ script.js
@@ -0,0 +1,8 @@
+//FUNCTIONAL CONSTRUCTOR
+//by using a functional constructor we can creates many objects
+var Person = function(name,birthYear,education,email){
+    this.name = name;
+    this.birthYear = birthYear;
+    this.education = education;
+    this.email = email;
+    }
